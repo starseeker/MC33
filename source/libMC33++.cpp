e44394edc1688a3965e8708fc1ba57cfa4aebd2b
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2024-09-17 15:19:56 -0400

    whitespace

--- source/libMC33++.cpp
+++ source/libMC33++.cpp
@@ -1,11 +1,11 @@
 /*
-	File: libMC33++.cpp
-	Programmed by: David Vega
-	August 2019
-	August 2020
-	April 2021
-	December 2021
-	Only this file must be included in a project to compile the MC33 library
+File: libMC33++.cpp
+Programmed by: David Vega
+August 2019
+August 2020
+April 2021
+December 2021
+Only this file must be included in a project to compile the MC33 library
 */
 
 #define compiling_libMC33
@@ -26,4 +26,14 @@ extern void (*mult_TSAbf)(const double (*)[3], MC33_real *, MC33_real *, int);
 #include "grid3d.cpp"
 #include "surface.cpp"
 #include "MC33.cpp"
+
+
+// Local Variables:
+// tab-width: 8
+// mode: C++
+// c-basic-offset: 4
+// indent-tabs-mode: t
+// c-file-style: "stroustrup"
+// End:
+// ex: shiftwidth=4 tabstop=8
 
